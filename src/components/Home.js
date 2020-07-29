49fbd0218ce02e67e1ebf580fa0852a69f58e655
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-07-29 15:35:48 -0400

    added styling for the about page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home-page">
       <h2>Welcome to The Power of Babel!</h2>
       <Link to="/app">
-        <img id="home-img" src="https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"/>
+        <img id="home-img" src="https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="a picture of computer code in a text editor"/>
       </Link>
       <h4>An application to find a Github user's favorite coding languages.</h4>
     </div>
