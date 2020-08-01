56e08224f5e3751eb81f00573e0e09dbde974878
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-08-01 11:01:11 -0400

    fix for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,6 @@ const Footer = () => {
 
   return(
     <footer>
-      <p>Styled by: <a href="https://github.com/joshuasoave">Josh Soave</a>
-      <span>Photo by <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Markus Spiske</a> on <a href="https://unsplash.com/s/photos/language?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-      </p>
     </footer>
   )
 
